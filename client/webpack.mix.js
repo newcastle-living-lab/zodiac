dc7a93f6c9942b62e606d3f26b431db4f08a0b2f
Craig Rodway <craig@cloud-systems.io>
2021-11-19 17:24:35 +0000

    Assets

--- client/webpack.mix.js
+++ client/webpack.mix.js
@@ -5,14 +5,23 @@ mix.setPublicPath('../server/public');
 
 let outPath = '../server/public';
 
+let outFilename = function(filename) {
+	if ( ! mix.inProduction()) return filename;
+	const parts = filename.split('.');
+	const ext = parts.pop();
+	return filename.replace(`.${ext}`, `.min.${ext}`);
+	// const basename = filename.replace(`.${ext}`, '');
+	// return [basename, 'min', ext].join('.');
+}
+
 // App
 //
-mix.sass('css/main.scss', outPath);
-mix.js('js/main.js', outPath);
+mix.sass('css/main.scss', Path.join(outPath, outFilename('main.css')));
+mix.js('js/main.js', Path.join(outPath, outFilename('main.js')));
 
 // HTMX + Hyperscript
 let htmxbundle = [
 	'node_modules/hyperscript.org/dist/_hyperscript.js',
 	'node_modules/htmx.org/dist/htmx.js',
 ];
-mix.scripts(htmxbundle, Path.join(outPath, 'htmxbundle.js'));
+mix.scripts(htmxbundle, Path.join(outPath, outFilename('htmxbundle.js')));
